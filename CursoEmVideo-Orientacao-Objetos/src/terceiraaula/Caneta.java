609cdbf0c0cdb912f27990345c9efcfe825ad9f9
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-15 11:28:23 -0300

    criacao do metodo status na classe Caneta.java para a terceira aula

--- CursoEmVideo-Orientacao-Objetos/src/terceiraaula/Caneta.java
+++ CursoEmVideo-Orientacao-Objetos/src/terceiraaula/Caneta.java
@@ -7,4 +7,12 @@ public class Caneta {
 	private Float ponta;
 	protected int carga;
 	private boolean tampada;
+	
+	public void status() {
+		System.out.println("Modelo: " + this.modelo);
+		System.out.println("Uma caneta " + this.cor);
+		System.out.println("Ponta: " + this.ponta);
+		System.out.println("Carga: " + this.carga);
+		System.out.println("Está tampada? " + this.tampada);
+	}
 }
