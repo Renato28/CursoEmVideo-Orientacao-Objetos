cdec050884e601bed20683cb47b9e6585b748d7c
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-15 14:03:24 -0300

    criacao dos metodos acessores getters e setters da classe Conta.java

--- CursoEmVideo-Orientacao-Objetos/src/quintaaula/Conta.java
+++ CursoEmVideo-Orientacao-Objetos/src/quintaaula/Conta.java
@@ -7,50 +7,90 @@ public class Conta {
 	protected String tipo;
 	private double saldo;
 	private boolean status;
-	
+
 	public Conta() {
-		
+
 		this.status = false;
 		this.saldo = 0;
 	}
-	
+
 	public void abrirConta() {
-		
+
 		this.status = true;
 	}
-	
+
 	public void fecharConta() {
-		
+
 	}
-	
+
 	public void sacar(double valor) {
-		
-		if(this.status == true && this.saldo > 0) {
-			
+
+		if (this.status == true && this.saldo > 0) {
+
 			this.saldo -= valor;
 		}
 	}
-	
+
 	public void depositar(double valor) {
-		
-		if(this.status == true) {
-			
+
+		if (this.status == true) {
+
 			this.saldo += valor;
 		}
-		
+
 	}
-	
+
 	public void pagarMensalidade() {
-		
-		if(this.tipo == "CP") {
-			
+
+		if (this.tipo == "CP") {
+
 			this.saldo = this.saldo - 20;
 		}
-		
-		if(this.tipo == "CC") {
-			
+
+		if (this.tipo == "CC") {
+
 			this.saldo = this.saldo - 12;
 		}
 	}
-	
+
+	public String getNomeDono() {
+		return nomeDono;
+	}
+
+	public void setNomeDono(String nomeDono) {
+		this.nomeDono = nomeDono;
+	}
+
+	public int getNumConta() {
+		return numConta;
+	}
+
+	public void setNumConta(int numConta) {
+		this.numConta = numConta;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
 }
