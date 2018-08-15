20450e90190fcc33a7f272a1c0fe68eeef896c96
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-15 11:40:35 -0300

    criacao dos metodos acessores na classe Caneta.java para a quarta aula

--- CursoEmVideo-Orientacao-Objetos/src/quartaaula/Caneta.java
+++ CursoEmVideo-Orientacao-Objetos/src/quartaaula/Caneta.java
@@ -31,4 +31,44 @@ public class Caneta {
 	public void destampar() {
 		this.tampada = false;
 	}
+
+	public String getModelo() {
+		return this.modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getCor() {
+		return this.cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public float getPonta() {
+		return this.ponta;
+	}
+
+	public void setPonta(float ponta) {
+		this.ponta = ponta;
+	}
+
+	public int getCarga() {
+		return this.carga;
+	}
+
+	public void setCarga(int carga) {
+		this.carga = carga;
+	}
+
+	public boolean isTampada() {
+		return this.tampada;
+	}
+
+	public void setTampada(boolean tampada) {
+		this.tampada = tampada;
+	}
 }
