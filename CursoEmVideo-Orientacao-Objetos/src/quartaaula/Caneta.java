46cc1cd42d1f9160008cfaf1ea8a3a3a7fa6d9a9
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-15 11:39:16 -0300

    criacao do metodo destampar na classe Caneta.java para a quarta aula

--- CursoEmVideo-Orientacao-Objetos/src/quartaaula/Caneta.java
+++ CursoEmVideo-Orientacao-Objetos/src/quartaaula/Caneta.java
@@ -7,7 +7,7 @@ public class Caneta {
 	private Float ponta;
 	protected int carga;
 	private boolean tampada;
-	
+
 	public void status() {
 		System.out.println("Modelo: " + this.modelo);
 		System.out.println("Uma caneta " + this.cor);
@@ -15,7 +15,7 @@ public class Caneta {
 		System.out.println("Carga: " + this.carga);
 		System.out.println("Está tampada? " + this.tampada);
 	}
-	
+
 	public void rabiscar() {
 		if (this.tampada == true) {
 			System.out.println("ERRO! não posso rabiscar!");
@@ -23,8 +23,12 @@ public class Caneta {
 			System.out.println("Estou Rabiscando...");
 		}
 	}
-	
+
 	public void tampar() {
 		this.tampada = true;
+	}
+
+	public void destampar() {
+		this.tampada = false;
 	}
 }
