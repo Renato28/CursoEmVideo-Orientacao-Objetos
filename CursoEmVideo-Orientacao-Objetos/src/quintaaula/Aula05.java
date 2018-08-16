43b833be4711a29cf8c35192a77ee5b5e5e0bb78
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-16 08:50:04 -0300

    criacao do segundo objeto para execucao dos conceitos de poo

--- CursoEmVideo-Orientacao-Objetos/src/quintaaula/Aula05.java
+++ CursoEmVideo-Orientacao-Objetos/src/quintaaula/Aula05.java
@@ -3,11 +3,16 @@ package quintaaula;
 public class Aula05 {
 
 	public static void main(String[] args) {
+		
 		Conta p1 = new Conta();
 		p1.setNumConta(1111);
 		p1.setNomeDono("Jubileu");
 		p1.abrirConta("CC");
-
+		
+		Conta p2 = new Conta();
+		p2.setNumConta(2222);
+		p2.setNomeDono("Creuza");
+		p2.abrirConta("CP");
 	}
 
 }
