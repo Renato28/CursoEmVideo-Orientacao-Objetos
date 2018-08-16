2bcb9cd2cdbd108edb8b9b45469857b7847575d2
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-16 12:56:30 -0300

    implementacao do metodo desligarMudo na classe ControleRemoto

--- CursoEmVideo-Orientacao-Objetos/src/sextaaula/ControleRemoto.java
+++ CursoEmVideo-Orientacao-Objetos/src/sextaaula/ControleRemoto.java
@@ -103,7 +103,10 @@ public class ControleRemoto implements Controlador{
 	@Override
 	public void desligarMudo() {
 		
-		
+		if((this.getLigado()) && this.getVolume() == 0) {
+			
+			this.setVolume(50);
+		}
 	}
 
 	@Override
