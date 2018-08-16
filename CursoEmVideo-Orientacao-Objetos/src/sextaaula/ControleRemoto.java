a89681380bbc4f996c54d470f408d2b7a6562fd7
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-16 12:35:26 -0300

    implementacao dos metodos abstratos da interface Controlador na classe ControleRemoto

--- CursoEmVideo-Orientacao-Objetos/src/sextaaula/ControleRemoto.java
+++ CursoEmVideo-Orientacao-Objetos/src/sextaaula/ControleRemoto.java
@@ -1,6 +1,6 @@
 package sextaaula;
 
-public class ControleRemoto {
+public class ControleRemoto implements Controlador{
 	
 	
 	private int volume;
@@ -35,6 +35,66 @@ public class ControleRemoto {
 	private boolean getTocando() {
 		
 		return this.tocando;
+	}
+
+	@Override
+	public void ligar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void desligar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void abrirMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void fecharMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void maisVolume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void menosVolume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ligarMudo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void desligarMudo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	
