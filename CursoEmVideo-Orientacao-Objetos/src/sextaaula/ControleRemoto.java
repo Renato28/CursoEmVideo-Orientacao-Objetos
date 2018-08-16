5ec0b0d2fdc35790d52cbefc630d5cf9c460909d
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-16 12:39:43 -0300

    criacao do metodo construtor na classe ControleRemoto

--- CursoEmVideo-Orientacao-Objetos/src/sextaaula/ControleRemoto.java
+++ CursoEmVideo-Orientacao-Objetos/src/sextaaula/ControleRemoto.java
@@ -7,6 +7,13 @@ public class ControleRemoto implements Controlador{
 	private boolean ligado;
 	private boolean tocando;
 	
+	public ControleRemoto() {
+		
+		this.setVolume(50);
+		this.setLigado(false);
+		this.setTocando(false);
+	}
+	
 	private void setVolume(int volume) {
 		
 		this.volume = volume;
@@ -39,61 +46,61 @@ public class ControleRemoto implements Controlador{
 
 	@Override
 	public void ligar() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void desligar() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void abrirMenu() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void fecharMenu() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void maisVolume() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void menosVolume() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void ligarMudo() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void desligarMudo() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void play() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 	
