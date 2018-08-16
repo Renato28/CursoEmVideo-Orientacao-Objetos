a89e3533231fc0bbc13759d3d2c76c285427cdf2
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-16 12:18:49 -0300

    criacao dos metodos acessores getters e setters na classe ControleRemoto

--- CursoEmVideo-Orientacao-Objetos/src/sextaaula/ControleRemoto.java
+++ CursoEmVideo-Orientacao-Objetos/src/sextaaula/ControleRemoto.java
@@ -2,7 +2,40 @@ package sextaaula;
 
 public class ControleRemoto {
 	
+	
 	private int volume;
 	private boolean ligado;
 	private boolean tocando;
+	
+	private void setVolume(int volume) {
+		
+		this.volume = volume;
+	}
+	
+	private int getVolume() {
+		
+		return this.volume;
+	}
+	
+	private void setLigado(boolean ligado) {
+		
+		this.ligado = ligado;
+	}
+	
+	private boolean getLigado() {
+		
+		return this.ligado;
+	}
+	
+	private void setTocando(boolean tocando) {
+		
+		this.tocando = tocando;
+	}
+	
+	private boolean getTocando() {
+		
+		return this.tocando;
+	}
+	
+	
 }
