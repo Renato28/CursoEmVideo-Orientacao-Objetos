83546aa5b0d06bf0fe99b0d476a6b5d79079734c
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-17 09:22:52 -0300

    execucao dos conceitos de poo

--- CursoEmVideo-Orientacao-Objetos/src/sextaaula/Aula06.java
+++ CursoEmVideo-Orientacao-Objetos/src/sextaaula/Aula06.java
@@ -5,7 +5,6 @@ public class Aula06 {
 	public static void main(String[] args) {
 		
 		ControleRemoto controle = new ControleRemoto();
-		controle.ligar();
 		controle.abrirMenu();
 		controle.play();
 		controle.maisVolume();
