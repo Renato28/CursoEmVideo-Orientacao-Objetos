6bc677ba646abedf0edacf21405465919d6184f5
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-22 10:25:07 -0300

    criacao dos atributos da classe lutador

--- CursoEmVideo-Orientacao-Objetos/src/setimaaula/Lutador.java
+++ CursoEmVideo-Orientacao-Objetos/src/setimaaula/Lutador.java
@@ -2,4 +2,13 @@ package setimaaula;
 
 public class Lutador {
 
+	private String nome;
+	private String nacionalidade;
+	private int idade;
+	private double altura;
+	private double peso;
+	private String categoria;
+	private int vitorias;
+	private int derrotas;
+	private int empates;
 }
