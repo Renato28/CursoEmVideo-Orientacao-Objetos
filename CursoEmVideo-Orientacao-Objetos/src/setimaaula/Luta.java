40ad73334b1b2d445b91cd6ce474b8d48d867a9e
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-22 12:12:54 -0300

    criacao do metodo construtor da classe luta

--- CursoEmVideo-Orientacao-Objetos/src/setimaaula/Luta.java
+++ CursoEmVideo-Orientacao-Objetos/src/setimaaula/Luta.java
@@ -8,4 +8,15 @@ public class Luta {
 	private String desafiante;
 	private int rounds;
 	private boolean aprovada;
+	
+	//Metodo construtor
+	
+	public Luta(String desafiado, String desafiante, int rounds) {
+		
+		this.desafiado = desafiado;
+		this.desafiante = desafiante;
+		this.rounds = rounds;
+	}
+	
+	
 }
