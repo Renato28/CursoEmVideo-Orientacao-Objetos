edc292c4ddb31b2ddc1aec058e7a31c34b7c7da0
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-22 11:22:35 -0300

    implementando o metodo acessor setCategoria da classe lutador

--- CursoEmVideo-Orientacao-Objetos/src/setimaaula/Lutador.java
+++ CursoEmVideo-Orientacao-Objetos/src/setimaaula/Lutador.java
@@ -15,9 +15,10 @@ public class Lutador {
 	private int empates;
 
 	// Metodo construtor
-	
-	public Lutador(String nome, String nacionalidade, int idade, double altura, double peso, int vitorias, int derrotas, int empates) {
-		
+
+	public Lutador(String nome, String nacionalidade, int idade, double altura, double peso, int vitorias, int derrotas,
+			int empates) {
+
 		this.nome = nome;
 		this.nacionalidade = nacionalidade;
 		this.idade = idade;
@@ -33,36 +34,32 @@ public class Lutador {
 	public void apresentar() {
 
 		System.out.println("Senhoras e Senhores, queremos apresentar o lutador " + this.getNome() + " ele veio do "
-				+ this.getNacionalidade() + " tem " + this.getIdade() + " anos " + " " + this.getAltura() + " m  de altura "
-				+ " pesa " + this.getPeso() + " kg " + " sua categoria é: " + this.getCategoria() + " tem " + this.getVitorias()
-				+ " vitórias " + " " + this.getDerrotas() + " derrotas " + " e " + this.getEmpates() + " empates ");
+				+ this.getNacionalidade() + " tem " + this.getIdade() + " anos " + " " + this.getAltura()
+				+ " m  de altura " + " pesa " + this.getPeso() + " kg " + " sua categoria é: " + this.getCategoria()
+				+ " tem " + this.getVitorias() + " vitórias " + " " + this.getDerrotas() + " derrotas " + " e "
+				+ this.getEmpates() + " empates ");
 	}
 
 	public void status() {
-		
-		System.out.println("Lutador: " + this.getNome() +
-							"Nacionalidade: " + this.getNacionalidade() +
-							"Idade: " + this.getIdade() +
-							"Altura: " + this.getAltura() +
-							"Peso: " + this.getPeso() +
-							"Categoria " + this.getCategoria() +
-							"Vitorias: " + this.getVitorias() + 
-							"Derrotas: " + this.getDerrotas() + 
-							"Empates: " + this.getEmpates());
+
+		System.out.println("Lutador: " + this.getNome() + "Nacionalidade: " + this.getNacionalidade() + "Idade: "
+				+ this.getIdade() + "Altura: " + this.getAltura() + "Peso: " + this.getPeso() + "Categoria "
+				+ this.getCategoria() + "Vitorias: " + this.getVitorias() + "Derrotas: " + this.getDerrotas()
+				+ "Empates: " + this.getEmpates());
 	}
 
 	public void ganharLuta() {
-		
+
 		this.setVitorias(this.getVitorias() + 1);
 	}
 
 	public void perderLuta() {
-		
+
 		this.setDerrotas(this.getDerrotas() + 1);
 	}
 
 	public void empatarLuta() {
-		
+
 		this.setEmpates(this.getEmpates() + 1);
 	}
 
@@ -114,7 +111,23 @@ public class Lutador {
 	}
 
 	public void setCategoria(String categoria) {
-		this.categoria = categoria;
+		
+		if (this.peso < 52.2) {
+
+			categoria = "Invalido";
+			
+		} else if (this.peso <= 70.3) {
+
+			categoria = "Leve";
+			
+		} else if (this.peso <= 83.9) {
+
+			categoria = "Médio";
+			
+		} else if (this.peso <= 120.2) {
+
+			categoria = "Pesado";
+		}
 	}
 
 	public int getVitorias() {
