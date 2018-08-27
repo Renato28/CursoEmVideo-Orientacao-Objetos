620ce15f208ed09b206f3664a627c09d9f93e80a
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-27 11:01:30 -0300

    criacao de um objeto da classe Pessoa

--- CursoEmVideo-Orientacao-Objetos/src/oitavaaula/ProjetoLivro.java
+++ CursoEmVideo-Orientacao-Objetos/src/oitavaaula/ProjetoLivro.java
@@ -4,7 +4,10 @@ public class ProjetoLivro {
 
 	public static void main(String[] args) {
 		
-		
+		Pessoa[] p = new Pessoa[3];
+		p[0] = new Pessoa("Renato", 30, 'M');
+		p[1] = new Pessoa("Fernanda", 22, 'F');
+		p[2] = new Pessoa("Andriel", 14, 'M');
 	}
 
 }
