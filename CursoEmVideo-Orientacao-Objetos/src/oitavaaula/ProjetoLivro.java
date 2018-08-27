a1778b651c3c6f91cbe824ab66cfbd410e5a487b
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-27 11:24:38 -0300

    criacao de um objeto da classe Livro

--- CursoEmVideo-Orientacao-Objetos/src/oitavaaula/ProjetoLivro.java
+++ CursoEmVideo-Orientacao-Objetos/src/oitavaaula/ProjetoLivro.java
@@ -8,6 +8,12 @@ public class ProjetoLivro {
 		p[0] = new Pessoa("Renato", 30, 'M');
 		p[1] = new Pessoa("Fernanda", 22, 'F');
 		p[2] = new Pessoa("Andriel", 14, 'M');
+		
+		Livro[] l = new Livro[3];
+		l[0] = new Livro("Iracema", "José de Alencar", 300, p[0]);
+		l[1] = new Livro("Saraminda", "José Sarney", 450, p[1]);
+		l[2] = new Livro("Brida", "Paulo Coelho", 250, p[2]);
+		
 	}
 
 }
