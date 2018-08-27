c9d25057a84e150a5f83b62dc562b4594d88f420
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-27 10:07:07 -0300

    criacao dos metodos acessores getters e setters

--- CursoEmVideo-Orientacao-Objetos/src/oitavaaula/Pessoa.java
+++ CursoEmVideo-Orientacao-Objetos/src/oitavaaula/Pessoa.java
@@ -5,10 +5,39 @@ public class Pessoa {
 	private String nome;
 	private int idade;
 	private char sexo;
-	
+
 	public void fazerAniver() {
-		
-		
+
 	}
-	
+
+	public void setNome(String nome) {
+
+		this.nome = nome;
+	}
+
+	public String getNome() {
+
+		return this.nome;
+	}
+
+	public void setIdade(int idade) {
+
+		this.idade = idade;
+
+	}
+
+	public int getIdade() {
+
+		return this.idade;
+	}
+
+	public void setSexo(char sexo) {
+
+		this.sexo = sexo;
+	}
+
+	public char getSexo() {
+
+		return this.sexo;
+	}
 }
