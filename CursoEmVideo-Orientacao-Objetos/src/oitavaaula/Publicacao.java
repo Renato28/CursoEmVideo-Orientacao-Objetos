af41a54384c906fcd6021250f3ad61ac133a4186
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-27 10:23:21 -0300

    criacao dos metodos na interface publicacao

--- CursoEmVideo-Orientacao-Objetos/src/oitavaaula/Publicacao.java
+++ CursoEmVideo-Orientacao-Objetos/src/oitavaaula/Publicacao.java
@@ -2,5 +2,13 @@ package oitavaaula;
 
 public interface Publicacao {
 
+	public void abrir();
 	
+	public void fechar();
+	
+	public void folhear();
+			
+	public void voltarPag();
+	
+	public void avancarPag();
 }
