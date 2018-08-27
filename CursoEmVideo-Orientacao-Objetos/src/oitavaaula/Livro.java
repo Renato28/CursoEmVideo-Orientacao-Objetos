fde46eb648fdb2ef993261e598481d00af79416f
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-27 10:19:16 -0300

    criacao dos metodos acessores getters e setters

--- CursoEmVideo-Orientacao-Objetos/src/oitavaaula/Livro.java
+++ CursoEmVideo-Orientacao-Objetos/src/oitavaaula/Livro.java
@@ -8,9 +8,68 @@ public class Livro {
 	private int pagAtual;
 	private boolean aberto;
 	private Pessoa leitor;
-	
+
 	public void detalhes() {
-		
-		
+
+	}
+
+	public void setTitulo(String titulo) {
+
+		this.titulo = titulo;
+	}
+
+	public String gettitulo() {
+
+		return this.titulo;
+	}
+
+	public void setAutor(String autor) {
+
+		this.autor = autor;
+	}
+
+	public String getAutor() {
+
+		return this.autor;
+	}
+
+	public void setTotPaginas(int totPaginas) {
+
+		this.totPaginas = totPaginas;
+	}
+
+	public int getTotPaginas() {
+
+		return this.totPaginas;
+	}
+
+	public void setPagAtual(int pagAtual) {
+
+		this.pagAtual = pagAtual;
+	}
+
+	public int getPagAtual() {
+
+		return this.pagAtual;
+	}
+
+	public void setAberto(boolean aberto) {
+
+		this.aberto = aberto;
+	}
+
+	public boolean isAberto() {
+
+		return this.aberto;
+	}
+
+	public void setLeitor(Pessoa leitor) {
+
+		this.leitor = leitor;
+	}
+
+	public Pessoa getLeitor() {
+
+		return this.leitor;
 	}
 }
