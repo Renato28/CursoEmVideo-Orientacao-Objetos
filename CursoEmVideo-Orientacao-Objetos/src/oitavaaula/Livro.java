478de56dbb3634e63946bb6c2688d53068985343
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-27 10:11:41 -0300

    criacao dos atributos da classe Livro

--- CursoEmVideo-Orientacao-Objetos/src/oitavaaula/Livro.java
+++ CursoEmVideo-Orientacao-Objetos/src/oitavaaula/Livro.java
@@ -2,4 +2,10 @@ package oitavaaula;
 
 public class Livro {
 
+	private String titulo;
+	private String autor;
+	private int totPaginas;
+	private int pagAtual;
+	private boolean aberto;
+	private Pessoa leitor;
 }
