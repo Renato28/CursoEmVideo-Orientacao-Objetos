a28a93a906e3a595594e99bf3aa7605820be374b
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-27 11:25:14 -0300

    execucao da classe ProjetoLivro

--- CursoEmVideo-Orientacao-Objetos/src/oitavaaula/ProjetoLivro.java
+++ CursoEmVideo-Orientacao-Objetos/src/oitavaaula/ProjetoLivro.java
@@ -14,6 +14,10 @@ public class ProjetoLivro {
 		l[1] = new Livro("Saraminda", "José Sarney", 450, p[1]);
 		l[2] = new Livro("Brida", "Paulo Coelho", 250, p[2]);
 		
+		System.out.println(l[0].detalhes());
+		System.out.println(l[1].detalhes());
+		System.out.println(l[2].detalhes());
+		
 	}
 
 }
