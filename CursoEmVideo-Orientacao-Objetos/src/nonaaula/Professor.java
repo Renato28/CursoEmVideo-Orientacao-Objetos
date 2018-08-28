683d5a53573e786255f01d695f310f93f3548de5
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-28 16:23:56 -0300

    inserindo comentarios

--- CursoEmVideo-Orientacao-Objetos/src/nonaaula/Professor.java
+++ CursoEmVideo-Orientacao-Objetos/src/nonaaula/Professor.java
@@ -1,20 +1,33 @@
 package nonaaula;
 
+/**
+ * 
+ * @author Renato Nobrega
+ *
+ */
 public class Professor {
-
+	
+	//atributos
+	
 	private String especialidade;
 	private double salario;
-
+	
+	//metodo construtor
+	
 	public Professor(String especialidade, double salario) {
 
 		this.especialidade = especialidade;
 		this.salario = salario;
 	}
-
+	
+	//metodos
+	
 	public void receberAumento() {
 
 	}
-
+	
+	// metodos getters e setters
+	
 	public String getEspecialidade() {
 		return especialidade;
 	}
