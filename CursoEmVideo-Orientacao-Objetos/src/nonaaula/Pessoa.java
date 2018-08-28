a77b12458d5ad1d2a8e02c41ff13dc07c2c4efaa
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-28 16:15:07 -0300

    criacao do metodo fazerAniversario

--- CursoEmVideo-Orientacao-Objetos/src/nonaaula/Pessoa.java
+++ CursoEmVideo-Orientacao-Objetos/src/nonaaula/Pessoa.java
@@ -7,10 +7,14 @@ public class Pessoa {
 	private String sexo;
 
 	public Pessoa(String nome, int idade, String sexo) {
-	
+
 		this.nome = nome;
 		this.idade = idade;
 		this.sexo = sexo;
+	}
+
+	public void fazerAniversario() {
+
 	}
 
 	public String getNome() {
