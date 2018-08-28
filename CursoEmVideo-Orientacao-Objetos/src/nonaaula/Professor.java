62c0bc159e7be1f7f718a86909449d75d958189c
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-28 16:20:46 -0300

    criando os metodos acessores getters e setters

--- CursoEmVideo-Orientacao-Objetos/src/nonaaula/Professor.java
+++ CursoEmVideo-Orientacao-Objetos/src/nonaaula/Professor.java
@@ -4,4 +4,21 @@ public class Professor {
 
 	private String especialidade;
 	private double salario;
+
+	public String getEspecialidade() {
+		return especialidade;
+	}
+
+	public void setEspecialidade(String especialidade) {
+		this.especialidade = especialidade;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
 }
