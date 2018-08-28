1d134560731f1da713369e3ba4fcf7905d4b24bb
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-28 16:29:31 -0300

    inserindo comentarios

--- CursoEmVideo-Orientacao-Objetos/src/nonaaula/Funcionario.java
+++ CursoEmVideo-Orientacao-Objetos/src/nonaaula/Funcionario.java
@@ -1,23 +1,33 @@
 package nonaaula;
 
+/**
+ * 
+ * @author Renato Nobrega
+ *
+ */
 public class Funcionario {
-
+	
+	//atributos
+	
 	private String setor;
 	private boolean trabalhando;
 	
+	//metodo construtor
 	
-
 	public Funcionario(String setor) {
-		
+
 		this.setor = setor;
 		this.trabalhando = false;
 	}
 	
+	//metodos
+	
 	public void mudarTrabalho() {
-		
-		
-	}
 
+	}
+	
+	//metodos getters e setters
+	
 	public String getSetor() {
 		return setor;
 	}
