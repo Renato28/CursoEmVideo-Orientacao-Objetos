4448b425a737b3b82fb88399075d9fdc21b4cc1b
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-28 16:35:06 -0300

    inserindo comentarios

--- CursoEmVideo-Orientacao-Objetos/src/nonaaula/Aluno.java
+++ CursoEmVideo-Orientacao-Objetos/src/nonaaula/Aluno.java
@@ -1,20 +1,33 @@
 package nonaaula;
 
+/**
+ * 
+ * @author Renato Nobrega
+ *
+ */
 public class Aluno {
-
+	
+	//atributos
+	
 	private int matricula;
 	private String curso;
-
+	
+	//metodo construtor
+	
 	public Aluno(int matricula, String curso) {
 
 		this.matricula = matricula;
 		this.curso = curso;
 	}
-
+	
+	//metodos
+	
 	public void cancelarMatricula() {
 
 	}
-
+	
+	//metodos getters e setters
+	
 	public int getMatricula() {
 		return matricula;
 	}
