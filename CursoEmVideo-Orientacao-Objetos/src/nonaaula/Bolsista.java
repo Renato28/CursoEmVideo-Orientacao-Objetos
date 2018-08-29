92ced8f0c0f2a2b362534a8d104edc9c19207114
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-29 12:57:14 -0300

    criando o metodo acessor getter e setter da classe Bolsista

--- CursoEmVideo-Orientacao-Objetos/src/nonaaula/Bolsista.java
+++ CursoEmVideo-Orientacao-Objetos/src/nonaaula/Bolsista.java
@@ -5,4 +5,16 @@ public class Bolsista extends Aluno {
 	//atributos
 	
 	private String bolsa;
+
+	//criando o metodo getter e setter
+	
+	public String getBolsa() {
+		return bolsa;
+	}
+
+	public void setBolsa(String bolsa) {
+		this.bolsa = bolsa;
+	}
+	
+	
 }
