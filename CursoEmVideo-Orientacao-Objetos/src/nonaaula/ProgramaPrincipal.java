aad0b0904d856e2c01e261a142f7390cac2dc17f
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-29 11:54:15 -0300

    inserindo comentarios na classe ProgramaPrincipal

--- CursoEmVideo-Orientacao-Objetos/src/nonaaula/ProgramaPrincipal.java
+++ CursoEmVideo-Orientacao-Objetos/src/nonaaula/ProgramaPrincipal.java
@@ -1,15 +1,30 @@
 package nonaaula;
 
+/**
+ * 
+ * @author Renato Nobrega
+ *
+ */
 public class ProgramaPrincipal {
 
 	public static void main(String[] args) {
-
+		
+		//criando um objeto da classe Pessoa e atribuindo valores aos seus atributos
+		
 		Pessoa pessoa = new Pessoa();
 		pessoa.setNome("Renato");
 		pessoa.setIdade(30);
 		pessoa.setSexo("M");
+		
+		//executando o metodo da classe Pessoa
+				
 		pessoa.fazerAniversario();
+		
+		//executando os valores dos atributos e metodos da classe Pessoa
+		
 		System.out.println(pessoa);
+		
+		//criando um objeto da classe Aluno e atribuindo valores aos seus atributos
 		
 		Aluno aluno = new Aluno();
 		aluno.setNome("Andriel");
@@ -17,9 +32,17 @@ public class ProgramaPrincipal {
 		aluno.setSexo("M");
 		aluno.setMatricula(123);
 		aluno.setCurso("Inglês");
+		
+		//executando os metodos da classe Aluno
+		
 		aluno.cancelarMatricula();
 		aluno.fazerAniversario();
+		
+		//executando os valores dos atributos e metodos da classe Aluno
+		
 		System.out.println(aluno);
+		
+		//criando um objeto da classe Funcionario e atribuindo valores aos seus atributos
 		
 		Funcionario funcionario = new Funcionario();
 		funcionario.setNome("Maria");
@@ -27,9 +50,17 @@ public class ProgramaPrincipal {
 		funcionario.setSexo("F");
 		funcionario.setSetor("Secretaria");
 		funcionario.setTrabalhando(true);
+		
+		//executando os metodos da classe Funcionario
+		
 		funcionario.fazerAniversario();
 		funcionario.mudarTrabalho();
+		
+		//executando os valores dos atributos e metodos da classe Funcionario
+		
 		System.out.println(funcionario);
+		
+		//criando um objeto da classe Professor e atribuindo valores aos seus atributos
 		
 		Professor professor = new Professor();
 		professor.setNome("Rodrigo");
@@ -37,8 +68,14 @@ public class ProgramaPrincipal {
 		professor.setSexo("M");
 		professor.setEspecialidade("Java");
 		professor.setSalario(2500);
+		
+		//executando os metodos da classe Professor
+		
 		professor.fazerAniversario();
 		professor.receberAumento(200);
+		
+		//executando os valores dos atributos e metodos da classe Professor
+		
 		System.out.println(professor);
 		
 	}
