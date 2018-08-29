39bac9243dedc8078b5734c72974d2310c38cd25
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-29 10:54:47 -0300

    criando objeto da classe pessoa e atribuindo valores aos seus atributos

--- CursoEmVideo-Orientacao-Objetos/src/nonaaula/ProgramaPrincipal.java
+++ CursoEmVideo-Orientacao-Objetos/src/nonaaula/ProgramaPrincipal.java
@@ -4,7 +4,12 @@ public class ProgramaPrincipal {
 
 	public static void main(String[] args) {
 
+		Pessoa pessoa = new Pessoa();
+		pessoa.setNome("Renato");
+		pessoa.setIdade(30);
+		pessoa.setSexo("M");
 		
+		System.out.println(pessoa);
 	}
 
 }
