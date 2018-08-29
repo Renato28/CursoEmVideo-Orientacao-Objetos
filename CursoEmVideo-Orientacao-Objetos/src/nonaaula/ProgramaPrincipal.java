f8204076f5859e6f8ccdc9f7ebf3e4542b1f6213
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-29 09:15:33 -0300

    comentando codigo na classe ProgramaPrincipal

--- CursoEmVideo-Orientacao-Objetos/src/nonaaula/ProgramaPrincipal.java
+++ CursoEmVideo-Orientacao-Objetos/src/nonaaula/ProgramaPrincipal.java
@@ -4,12 +4,12 @@ public class ProgramaPrincipal {
 
 	public static void main(String[] args) {
 		
-		//instanciando classes e criando objetos
+		//codigo comentado
 		
-		Pessoa p1 = new Pessoa("Renato", 30, "M");
+		/*Pessoa p1 = new Pessoa("Renato", 30, "M");
 		Aluno p2 = new Aluno("Andriel", 14, "M");
 		Professor p3 = new Professor("Rodrigo", 35, "M");
-		Funcionario p4 = new Funcionario("Maria", 32, "F");
+		Funcionario p4 = new Funcionario("Maria", 32, "F");*/
 		
 		//exectando os metodos das classes
 		
