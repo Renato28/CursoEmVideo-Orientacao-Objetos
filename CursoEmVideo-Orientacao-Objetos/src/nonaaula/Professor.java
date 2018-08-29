b56ab43f7b78573f3a285260bbeb1d184d4a2586
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-29 10:03:49 -0300

    comentando codigo na classe Professor

--- CursoEmVideo-Orientacao-Objetos/src/nonaaula/Professor.java
+++ CursoEmVideo-Orientacao-Objetos/src/nonaaula/Professor.java
@@ -44,12 +44,14 @@ public class Professor extends Pessoa {
 	public void setSalario(double salario) {
 		this.salario = salario;
 	}
-
-	@Override
+	
+	//codigo comentado
+	
+	/*@Override
 	public String toString() {
 		
 		return "Professor [especialidade=" + especialidade + ", salario=" + salario + "]";
-	}
+	}*/
 	
 	
 
