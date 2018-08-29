e4d0ebe4efc8f08ffd2a9c5384eebd663a977f38
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-29 08:54:10 -0300

    instanciando classes e criando objetos

--- CursoEmVideo-Orientacao-Objetos/src/nonaaula/ProgramaPrincipal.java
+++ CursoEmVideo-Orientacao-Objetos/src/nonaaula/ProgramaPrincipal.java
@@ -4,7 +4,12 @@ public class ProgramaPrincipal {
 
 	public static void main(String[] args) {
 		
+		//instanciando classes e criando objetos
 		
+		Pessoa p1 = new Pessoa("Renato", 30, "M");
+		Aluno p2 = new Aluno("Andriel", 14, "M");
+		Professor p3 = new Professor("Rodrigo", 35, "M");
+		Funcionario p4 = new Funcionario("Maria", 32, "F");
 	}
 
 }
