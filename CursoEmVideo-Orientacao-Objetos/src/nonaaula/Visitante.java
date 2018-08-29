139cef4c20097b868c2a7603bb247140aa1cde4f
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-29 13:15:02 -0300

    implementando o metodo toString com o StringBuffer

--- CursoEmVideo-Orientacao-Objetos/src/nonaaula/Visitante.java
+++ CursoEmVideo-Orientacao-Objetos/src/nonaaula/Visitante.java
@@ -1,6 +1,28 @@
 package nonaaula;
 
+/**
+ * 
+ * @author Renato Nobrega
+ *
+ */
 public class Visitante extends Pessoa{
+	
+	//implementando o metodo tostring com o StringBuffer
+	
+	@Override
+	public String toString() {
+		
+		StringBuffer stringBuffer = new StringBuffer();
+		stringBuffer.append("Visitante: ");
+		stringBuffer.append(" Nome: ");
+		stringBuffer.append(this.getNome());
+		stringBuffer.append(", Idade: ");
+		stringBuffer.append(this.getIdade());
+		stringBuffer.append(", Sexo: ");
+		stringBuffer.append(this.getSexo());
+		
+		return stringBuffer.toString();
+	}
 
 	
 }
