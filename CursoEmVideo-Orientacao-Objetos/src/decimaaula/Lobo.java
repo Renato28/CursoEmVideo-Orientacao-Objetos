c3805c8a2af7d5696207a16a40dba7e01ea58d90
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-30 18:25:49 -0300

    implementacao do metodo de sobreposicao na classe Lobo

--- CursoEmVideo-Orientacao-Objetos/src/decimaaula/Lobo.java
+++ CursoEmVideo-Orientacao-Objetos/src/decimaaula/Lobo.java
@@ -7,5 +7,12 @@ package decimaaula;
  */
 public class Lobo extends Mamifero {
 
+	//sobreposicao do metodo emitirSom da classe Mamifero
 	
+	@Override
+	
+	public void emitirSom() {
+		
+		System.out.println("auuuuuuuuuu!!!");
+	}
 }
