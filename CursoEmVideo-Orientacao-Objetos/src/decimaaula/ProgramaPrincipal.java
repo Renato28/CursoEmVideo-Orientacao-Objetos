c1c35e80db87990dd29480538bffb45e724e45c1
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-30 13:55:48 -0300

    executando a classe Reptil

--- CursoEmVideo-Orientacao-Objetos/src/decimaaula/ProgramaPrincipal.java
+++ CursoEmVideo-Orientacao-Objetos/src/decimaaula/ProgramaPrincipal.java
@@ -9,7 +9,7 @@ public class ProgramaPrincipal {
 
 	public static void main(String[] args) {
 		
-		//criando um objeto e atribuindo valores aos atributos
+		//criando um objeto e atribuindo valores aos atributos da classe Mamifero
 		
 		Mamifero mamifero = new Mamifero();
 		mamifero.setPeso(20.5);
@@ -23,9 +23,27 @@ public class ProgramaPrincipal {
 		mamifero.emitirSom();
 		mamifero.locomover();
 		
-		//executando os valores dos atributos e metodos da classe Mamifero
+		//executando os valores dos atributos da classe Mamifero
 		
 		System.out.println(mamifero);
+		
+		//criando um objeto e atribuindo valores aos atributos da classe Reptil
+		
+		Reptil reptil = new Reptil();
+		reptil.setPeso(12.7);
+		reptil.setIdade(5);
+		reptil.setMembros(4);
+		reptil.setCorDaEscama("marrom");
+		
+		//executando os metodos da classe Reptil
+		
+		reptil.alimentar();
+		reptil.emitirSom();
+		reptil.locomover();
+		
+		//executando os valores dos atributos da classe Reptil
+		
+		System.out.println(reptil);
 	}
 
 }
