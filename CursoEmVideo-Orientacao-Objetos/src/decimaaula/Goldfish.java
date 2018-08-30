aac0fbe8b026cb8fea4afbae3dc55817f0952e08
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-30 13:38:50 -0300

    implementando o metodo toString na classe Goldfish

--- CursoEmVideo-Orientacao-Objetos/src/decimaaula/Goldfish.java
+++ CursoEmVideo-Orientacao-Objetos/src/decimaaula/Goldfish.java
@@ -5,7 +5,13 @@ package decimaaula;
  * @author Renato Nobrega
  *
  */
-public class Goldfish {
-
+public class Goldfish extends Peixe {
 	
+	//metodo toString
+	
+	@Override
+	public String toString() {
+		return "Peso: " + this.peso + " idade: " + this.idade + " membros: " + this.membros;
+	}
+
 }
