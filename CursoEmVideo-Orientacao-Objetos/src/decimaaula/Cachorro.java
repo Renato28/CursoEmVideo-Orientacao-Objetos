771734b58f685e2533694ad3c99ff3319c140126
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-30 13:11:36 -0300

    implementando os metodos da classe Cachorro

---
+++ CursoEmVideo-Orientacao-Objetos/src/decimaaula/Cachorro.java
@@ -0,0 +1,21 @@
+package decimaaula;
+
+/**
+ * 
+ * @author Renato Nobrega
+ *
+ */
+public class Cachorro {
+
+	//metodos
+	
+	public void enterrarOsso() {
+		
+		System.out.println("enterrando osso");
+	}
+	
+	public void abanarRabo() {
+		
+		System.out.println("abanando o rabo");
+	}
+}
