cfd64fa219a50c41aa986337800e642a9a3d0a87
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-30 12:47:18 -0300

    criacao do metodo getter e setter da classe Peixe

--- CursoEmVideo-Orientacao-Objetos/src/decimaaula/Peixe.java
+++ CursoEmVideo-Orientacao-Objetos/src/decimaaula/Peixe.java
@@ -6,8 +6,19 @@ package decimaaula;
  *
  */
 public class Peixe {
-	
-	//atributos
-	
+
+	// atributos
+
 	protected String corDaEscama;
+	
+	//metodo getter e setter
+	
+	public String getCorDaEscama() {
+		return corDaEscama;
+	}
+
+	public void setCorDaEscama(String corDaEscama) {
+		this.corDaEscama = corDaEscama;
+	}
+
 }
