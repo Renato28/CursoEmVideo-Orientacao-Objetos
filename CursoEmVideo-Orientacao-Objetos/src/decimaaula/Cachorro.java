e87936af7687506b85fbf20e2ca8c1de85502804
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-30 18:42:12 -0300

    implementacao dos metodos de sobrecarga na classe Cachorro

--- CursoEmVideo-Orientacao-Objetos/src/decimaaula/Cachorro.java
+++ CursoEmVideo-Orientacao-Objetos/src/decimaaula/Cachorro.java
@@ -18,36 +18,83 @@ public class Cachorro extends Lobo {
 
 		System.out.println("abanando o rabo");
 	}
-	
-	//sobrecarga de metodos
-	
+
+	// sobrecarga de metodos
+
 	public void reagir(String frase) {
-		
+
+		if (frase.equals("toma comida") || frase.equals("olá")) {
+
+			System.out.println("Abanar e latir");
+
+		} else {
+
+			System.out.println("Rosnar");
+		}
 	}
-	
+
 	public void reagir(int hora, int minuto) {
-		
+
+		if (hora < 12) {
+
+			System.out.println("Abanar");
+
+		} else if (hora >= 18) {
+
+			System.out.println("Ignorar");
+
+		} else {
+
+			System.out.println("Abanar e Latir");
+		}
 	}
-	
+
 	public void reagir(boolean dono) {
-		
+
+		if (dono == true) {
+
+			System.out.println("Abanar");
+
+		} else {
+
+			System.out.println("Rosnar e Latir");
+		}
 	}
-	
-	public void reagir (int idade, double peso) {
-		
+
+	public void reagir(int idade, double peso) {
+
+		if (idade < 5) {
+			if (peso < 10) {
+
+				System.out.println("Abanar");
+
+			} else {
+
+				System.out.println("Latir");
+			}
+		} else {
+			if (peso < 10) {
+
+				System.out.println("Rosnar");
+
+			} else {
+
+				System.out.println("Ignorar");
+			}
+		}
 	}
-	
+
 	@Override
-	
-	//sobreposicao de metodo da classe Mamifero
-	
+
+	// sobreposicao de metodo da classe Mamifero
+
 	public void emitirSom() {
-		
+
 		System.out.println("Au! Au! Au!");
 	}
-	
-	//metodo toString
-	
+
+	// metodo toString
+
 	@Override
 	public String toString() {
 		return "Peso: " + this.peso + " idade: " + this.idade + " membros: " + this.membros;
