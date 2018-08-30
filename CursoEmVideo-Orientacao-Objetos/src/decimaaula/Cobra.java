29a2ade80cb878745425ce3cc511b2b79c3dbf9a
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-30 13:30:08 -0300

    implementacao do metodo toString na classe Cobra

--- CursoEmVideo-Orientacao-Objetos/src/decimaaula/Cobra.java
+++ CursoEmVideo-Orientacao-Objetos/src/decimaaula/Cobra.java
@@ -5,7 +5,13 @@ package decimaaula;
  * @author Renato Nobrega
  *
  */
-public class Cobra {
+public class Cobra extends Reptil {
 
-	
+	// metodo toString
+
+	@Override
+	public String toString() {
+		return "Peso: " + this.peso + " idade: " + this.idade + " membros: " + this.membros;
+	}
+
 }
