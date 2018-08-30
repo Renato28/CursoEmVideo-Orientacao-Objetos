9f04d74ee65a5b0cd6697c50f46f8a1d2037e490
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-30 13:16:21 -0300

    implementando o metodo toString

--- CursoEmVideo-Orientacao-Objetos/src/decimaaula/Cachorro.java
+++ CursoEmVideo-Orientacao-Objetos/src/decimaaula/Cachorro.java
@@ -5,17 +5,25 @@ package decimaaula;
  * @author Renato Nobrega
  *
  */
-public class Cachorro {
+public class Cachorro extends Mamifero {
 
-	//metodos
-	
+	// metodos
+
 	public void enterrarOsso() {
-		
+
 		System.out.println("enterrando osso");
 	}
-	
+
 	public void abanarRabo() {
-		
+
 		System.out.println("abanando o rabo");
 	}
+	
+	//metodo toString
+	
+	@Override
+	public String toString() {
+		return "Peso: " + this.peso + " idade: " + this.idade + " membros: " + this.membros;
+	}
+
 }
