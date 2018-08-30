642106f9be3dd62e8cb46bbac7b573d4e0464b72
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-30 13:26:06 -0300

    implementando o metodo toString

--- CursoEmVideo-Orientacao-Objetos/src/decimaaula/Canguru.java
+++ CursoEmVideo-Orientacao-Objetos/src/decimaaula/Canguru.java
@@ -7,19 +7,27 @@ package decimaaula;
  */
 public class Canguru extends Mamifero {
 
-	//metodos
-	
+	// metodos
+
 	public void usarBolsa() {
-		
+
 		System.out.println("usando bolsa");
 	}
-	
-	//sobreposicao de metodo da classe Mamifero
-	
+
+	// sobreposicao de metodo da classe Mamifero
+
 	@Override
-	
+
 	public void locomover() {
-		
+
 		System.out.println("saltando");
 	}
+	
+	//metodo toString
+	
+	@Override
+	public String toString() {
+		return "Peso: " + this.peso + " idade: " + this.idade + " membros: " + this.membros;
+	}
+
 }
