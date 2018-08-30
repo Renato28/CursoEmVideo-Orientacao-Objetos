e003c3697988cb1f3c229fb3a127fc27fceec827
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-30 13:48:36 -0300

    executando a classe Mamifero

--- CursoEmVideo-Orientacao-Objetos/src/decimaaula/ProgramaPrincipal.java
+++ CursoEmVideo-Orientacao-Objetos/src/decimaaula/ProgramaPrincipal.java
@@ -9,6 +9,23 @@ public class ProgramaPrincipal {
 
 	public static void main(String[] args) {
 		
+		//criando um objeto e atribuindo valores aos atributos
+		
+		Mamifero mamifero = new Mamifero();
+		mamifero.setPeso(20.5);
+		mamifero.setIdade(10);
+		mamifero.setMembros(4);
+		mamifero.setCorDoPelo("preto");
+		
+		//executando os metodos da classe Mamifero
+		
+		mamifero.alimentar();
+		mamifero.emitirSom();
+		mamifero.locomover();
+		
+		//executando os valores dos atributos e metodos da classe Mamifero
+		
+		System.out.println(mamifero);
 	}
 
 }
