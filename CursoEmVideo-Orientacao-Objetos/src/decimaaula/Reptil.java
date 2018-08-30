6f96983edaee69926779f6c06142da426bf27d81
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-30 12:12:29 -0300

    criando o metodo getter e setter da classe Reptil

--- CursoEmVideo-Orientacao-Objetos/src/decimaaula/Reptil.java
+++ CursoEmVideo-Orientacao-Objetos/src/decimaaula/Reptil.java
@@ -7,7 +7,18 @@ package decimaaula;
  */
 public class Reptil {
 
-	//atributos
-	
+	// atributos
+
 	protected String corDaEscama;
+	
+	//metodo getter e setter
+
+	public String getCorDaEscama() {
+		return corDaEscama;
+	}
+
+	public void setCorDaEscama(String corDaEscama) {
+		this.corDaEscama = corDaEscama;
+	}
+
 }
