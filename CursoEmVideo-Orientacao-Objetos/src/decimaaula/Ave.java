53ce57f6cff748dc87f714224e18e9f494a0fcf1
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-30 12:59:49 -0300

    implementando os metodos abstratos da classe Animal

--- CursoEmVideo-Orientacao-Objetos/src/decimaaula/Ave.java
+++ CursoEmVideo-Orientacao-Objetos/src/decimaaula/Ave.java
@@ -5,20 +5,37 @@ package decimaaula;
  * @author Renato Nobrega
  *
  */
-public class Ave {
+public class Ave extends Animal {
 
 	// atributos
 
 	protected String corPena;
-	
-	//metodo getter e setter
-	
+
+	// metodo getter e setter
+
 	public String getCorPena() {
 		return corPena;
 	}
 
 	public void setCorPena(String corPena) {
 		this.corPena = corPena;
+	}
+
+	// sobreposicao dos metodos abstratos da classe Animal
+
+	@Override
+	public void locomover() {
+
+	}
+
+	@Override
+	public void alimentar() {
+
+	}
+
+	@Override
+	public void emitirSom() {
+
 	}
 
 }
