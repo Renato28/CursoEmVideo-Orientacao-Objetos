0e498e4be7a8329d9f38fe46ff294879b6e7a45c
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-30 12:02:05 -0300

    implementando a sobreposicao dos metodos abstratos da classe Animal

--- CursoEmVideo-Orientacao-Objetos/src/decimaaula/Mamifero.java
+++ CursoEmVideo-Orientacao-Objetos/src/decimaaula/Mamifero.java
@@ -5,13 +5,13 @@ package decimaaula;
  * @author Renato Nobrega
  *
  */
-public class Mamifero {
+public class Mamifero extends Animal {
 
 	// atributos
 
 	protected String corDoPelo;
-	
-	//metodo getter e setter
+
+	// metodo getter e setter
 
 	public String getCorDoPelo() {
 		return corDoPelo;
@@ -19,6 +19,23 @@ public class Mamifero {
 
 	public void setCorDoPelo(String corDoPelo) {
 		this.corDoPelo = corDoPelo;
+	}
+
+	// sobreposicao dos metodos abstratos da classe Animal
+
+	@Override
+	public void locomover() {
+
+	}
+
+	@Override
+	public void alimentar() {
+
+	}
+
+	@Override
+	public void emitirSom() {
+
 	}
 
 }
