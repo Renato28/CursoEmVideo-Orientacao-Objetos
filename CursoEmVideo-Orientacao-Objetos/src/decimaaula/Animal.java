85761c8f99d5fb5a12599b989e1bafc6c21f1b17
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-30 11:47:32 -0300

    criando os metodos acessores getters e setters da classe Animal

--- CursoEmVideo-Orientacao-Objetos/src/decimaaula/Animal.java
+++ CursoEmVideo-Orientacao-Objetos/src/decimaaula/Animal.java
@@ -7,10 +7,36 @@ package decimaaula;
  */
 public abstract class Animal {
 
-	//atributos
-	
+	// atributos
+
 	protected double peso;
 	protected int idade;
 	protected int membros;
 	
+	//metodos getters e setters
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public int getMembros() {
+		return membros;
+	}
+
+	public void setMembros(int membros) {
+		this.membros = membros;
+	}
+
 }
