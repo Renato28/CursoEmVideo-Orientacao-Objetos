770b85339112db862874a71171b998e2f8ff58f0
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-30 13:35:38 -0300

    implementando o metodo toString na classe Tartaruga

--- CursoEmVideo-Orientacao-Objetos/src/decimaaula/Tartaruga.java
+++ CursoEmVideo-Orientacao-Objetos/src/decimaaula/Tartaruga.java
@@ -6,12 +6,20 @@ package decimaaula;
  *
  */
 public class Tartaruga extends Reptil {
-	
-	//sobreposicao do metodo da classe Reptil
-	
+
+	// sobreposicao do metodo da classe Reptil
+
 	@Override
 	public void locomover() {
-		
+
 		System.out.println("andando beeeem devagar!!!");
 	}
+
+	// metodo toString
+
+	@Override
+	public String toString() {
+		return "Peso: " + this.peso + " idade: " + this.idade + " membros: " + this.membros;
+	}
+
 }
