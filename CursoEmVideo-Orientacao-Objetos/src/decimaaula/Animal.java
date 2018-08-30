75f1fddac7bfaacbce67564b3d81b53b0d5e8d7f
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-30 11:48:52 -0300

    criando o metodo toString da classe Animal

--- CursoEmVideo-Orientacao-Objetos/src/decimaaula/Animal.java
+++ CursoEmVideo-Orientacao-Objetos/src/decimaaula/Animal.java
@@ -38,5 +38,15 @@ public abstract class Animal {
 	public void setMembros(int membros) {
 		this.membros = membros;
 	}
+	
+	//metodo toString
+	
+	@Override
+	public String toString() {
+		
+		return "Animal [peso=" + peso + ", idade=" + idade + ", membros=" + membros + "]";
+	}
+	
+	
 
 }
