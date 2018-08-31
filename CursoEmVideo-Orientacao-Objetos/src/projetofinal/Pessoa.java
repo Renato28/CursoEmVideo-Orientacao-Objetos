c137f5ec80f4931a1b226512c640c895b18814a0
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-31 12:38:15 -0300

    implementação de construtor na classe Pessoa

--- CursoEmVideo-Orientacao-Objetos/src/projetofinal/Pessoa.java
+++ CursoEmVideo-Orientacao-Objetos/src/projetofinal/Pessoa.java
@@ -13,6 +13,16 @@ public abstract class Pessoa {
 	protected int idade;
 	protected String sexo;
 	protected String experiencia;
+	
+	//construtor
+
+	public Pessoa(String nome, int idade, String sexo, String experiencia) {
+		super();
+		this.nome = nome;
+		this.idade = idade;
+		this.sexo = sexo;
+		this.experiencia = experiencia;
+	}
 
 	// metodos getters e setters
 
