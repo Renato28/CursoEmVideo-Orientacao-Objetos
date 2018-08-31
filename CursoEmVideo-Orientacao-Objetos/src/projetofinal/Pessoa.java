260f6580f8aff0f03ae53a81b3842686af4086f1
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-31 10:33:42 -0300

    implementação dos metodos acessores da classe Pessoa

--- CursoEmVideo-Orientacao-Objetos/src/projetofinal/Pessoa.java
+++ CursoEmVideo-Orientacao-Objetos/src/projetofinal/Pessoa.java
@@ -13,11 +13,11 @@ public abstract class Pessoa {
 	protected int idade;
 	protected String sexo;
 	protected String experiencia;
-	
-	//construtor
-	
+
+	// construtor
+
 	public Pessoa(String nome, int idade, String sexo, String experiencia) {
-		
+
 		super();
 		this.nome = nome;
 		this.idade = idade;
@@ -25,6 +25,38 @@ public abstract class Pessoa {
 		this.experiencia = experiencia;
 	}
 
-	
+	// metodos getters e setters
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getExperiencia() {
+		return experiencia;
+	}
+
+	public void setExperiencia(String experiencia) {
+		this.experiencia = experiencia;
+	}
 
 }
