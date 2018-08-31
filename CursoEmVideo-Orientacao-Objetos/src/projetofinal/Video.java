2e01d167e7231b4569a5b13d4c16226d47bd57f3
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-31 11:11:36 -0300

    implementação dos metodos acessores da classe Video

--- CursoEmVideo-Orientacao-Objetos/src/projetofinal/Video.java
+++ CursoEmVideo-Orientacao-Objetos/src/projetofinal/Video.java
@@ -7,13 +7,54 @@ package projetofinal;
  */
 public class Video {
 
-	//atributos
-	
+	// atributos
+
 	private String titulo;
 	private int avaliacao;
 	private int views;
 	private int curtidas;
 	private boolean reproduzindo;
 	
+	//metodos getters e setters
 	
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public int getAvaliacao() {
+		return avaliacao;
+	}
+
+	public void setAvaliacao(int avaliacao) {
+		this.avaliacao = avaliacao;
+	}
+
+	public int getViews() {
+		return views;
+	}
+
+	public void setViews(int views) {
+		this.views = views;
+	}
+
+	public int getCurtidas() {
+		return curtidas;
+	}
+
+	public void setCurtidas(int curtidas) {
+		this.curtidas = curtidas;
+	}
+
+	public boolean isReproduzindo() {
+		return reproduzindo;
+	}
+
+	public void setReproduzindo(boolean reproduzindo) {
+		this.reproduzindo = reproduzindo;
+	}
+
 }
