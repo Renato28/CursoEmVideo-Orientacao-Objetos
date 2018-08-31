cd9bb862bcfd0f6bfce8e8193e58371054261c4b
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-31 14:08:48 -0300

    implementação dos metodos acessores da classe Visualização

--- CursoEmVideo-Orientacao-Objetos/src/projetofinal/Visualizacao.java
+++ CursoEmVideo-Orientacao-Objetos/src/projetofinal/Visualizacao.java
@@ -11,12 +11,30 @@ public class Visualizacao {
 
 	private Gafanhoto espectador;
 	private String filme;
-	
-	//construtor
-	
+
+	// construtor
+
 	public Visualizacao(Gafanhoto espectador, String filme) {
-		
+
 		this.espectador = espectador;
+		this.filme = filme;
+	}
+	
+	//metodos getters e setters
+	
+	public Gafanhoto getEspectador() {
+		return espectador;
+	}
+
+	public void setEspectador(Gafanhoto espectador) {
+		this.espectador = espectador;
+	}
+
+	public String getFilme() {
+		return filme;
+	}
+
+	public void setFilme(String filme) {
 		this.filme = filme;
 	}
 
