66aadfbe7852a4c5a7f9572d2fb45583553c7704
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-31 12:21:49 -0300

    criação de vetor e atribuição de valores

--- CursoEmVideo-Orientacao-Objetos/src/projetofinal/ProjetoYoutube.java
+++ CursoEmVideo-Orientacao-Objetos/src/projetofinal/ProjetoYoutube.java
@@ -9,6 +9,12 @@ public class ProjetoYoutube {
 
 	public static void main(String[] args) {
 		
+		//criando um vetor e adicionando valores ao objeto
+		
+		Video[] videos = new Video[3];
+		videos[0] = new Video("Aula 01 de POO");
+		videos[1] = new Video("Aula 12 de JAVA");
+		videos[2] = new Video("Aula 10 de HTML5");
 		
 	}
 
